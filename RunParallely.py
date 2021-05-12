91955ab2b54554a561a20616fb8f53c1d2ca244e
pritishmishra703 <pritishjan@gmail.com>
2021-05-12 13:11:11 +0530

    RunParallely.py fully working!

--- RunParallely.py
+++ RunParallely.py
@@ -1,21 +1,57 @@
+###### IMPORTS ###################
 import threading
 import time
-import pyaudio
+import sounddevice as sd
+import librosa
+import numpy as np
+from tensorflow.keras.models import load_model
+import pyttsx3
 
-class Listener:
+#### SETTING UP TEXT TO SPEECH ###
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0')
 
-    def __init__(self, sample_rate=8000, record_seconds=2):
-        self.chunk = 1024
-        self.sample_rate = sample_rate
-        self.record_seconds = record_seconds
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(format=pyaudio.paInt16,
-                        channels=1,
-                        rate=self.sample_rate,
-                        input=True,
-                        output=True,
-                        frames_per_buffer=self.chunk)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    engine.endLoop()
 
-        print(self.stream.read(self.chunk, exception_on_overflow=False))
+##### CONSTANTS ################
+fs = 22050
+seconds = 2
 
-listen = Listener()
+model = load_model("D:\pritish\ChatBot\WakeWordDetection\saved_model\WWD2.h5")
+
+##### LISTENING THREAD #########
+def listener():
+    while True:
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+        sd.wait()
+        mfcc = librosa.feature.mfcc(y=myrecording.ravel(), sr=fs, n_mfcc=40)
+        mfcc_processed = np.mean(mfcc.T, axis=0)
+        prediction_thread(mfcc_processed)
+        time.sleep(0.001)
+
+
+def voice_thread():
+    listen_thread = threading.Thread(target=listener, name="ListeningFunction")
+    listen_thread.start()
+
+##### PREDICTION THREAD #############
+def prediction(y):
+    prediction = model.predict(np.expand_dims(y, axis=0))
+    if prediction[:, 1] > 0.96:
+        if engine._inLoop:
+            engine.endLoop()
+
+        speak("Hello, What can I do for you?")
+        
+
+    time.sleep(0.1)
+
+def prediction_thread(y):
+    pred_thread = threading.Thread(target=prediction, name="PredictFunction", args=(y,))
+    pred_thread.start()
+
+voice_thread()
