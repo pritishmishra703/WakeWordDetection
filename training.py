1128e97b6c0f90142c4d8b10314588a14bc4f719
Pritish Mishra <pritishjan@gmail.com>
2021-07-27 17:40:31 +0530

    PCM error issue solved

--- training.py
+++ training.py
@@ -6,7 +6,7 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from sklearn.metrics import confusion_matrix, classification_report
-from PCM.PCM import plot_confusion_matrix
+from plot_cm import plot_confusion_matrix
 
 ##### Loading saved csv ##############
 df = pd.read_pickle("final_audio_data_csv/audio_data.csv")
